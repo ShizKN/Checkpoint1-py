a3b206830d5bae9233bd4ccba8e2e71b438a7a34
ShizKN <twitchsavix@gmail.com>
2026-01-17 14:37:22 +0100

    commit

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 import random
-number = -65
+number = None
 print("Welcome to the Guess the Number game!")
 print("I'm thinking of a number between 1 and 100. Can you guess what it is?")
 guesses = 0
-guess = 0
+guess = None
 while True:
     choice = input('What range do you want? '
-                   '\n Press "A" for 0 to 1 '
-                   '\n Press "B" for 1 to 10'
-                   '\n Press "C" for 1 to 100'
-                   '\n Enter your choice: ')
-    if choice == "A".lower():
+'\n Press "A" for 0 to 1 '
+'\n Press "B" for 1 to 10'
+'\n Press "C" for 1 to 100'
+'\n Enter your choice:')
+    choice = choice.strip().lower()
+    if choice == "a":
         number = random.randint(0, 1)
-    elif choice == "B".lower():
+    elif choice == "b":
         number = random.randint(0, 10)
-    elif choice == "C".lower():
+    elif choice == "c":
         number = random.randint(0, 100)
     else:
         print("PLEASE ENTER A VALID CHOICE!")
